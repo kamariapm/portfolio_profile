cd14b9a778dfa76a0b14b19acce415a24275a21e
kamariapm <kamariapm@gmail.com>
2022-08-19 12:40:54 -0400

    added files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import "../styles/Footer.css";
 
 function Footer() {
@@ -10,11 +11,13 @@ function Footer() {
     <div className="footer">
       <div className="socialMedia">
         <InstagramIcon />
+        {<a href="https://www.w3schools.com"></a>}
         <TwitterIcon />
         <FacebookIcon />
         <LinkedInIcon />
+        <MailOutlineIcon />
       </div>
-      <p> &copy; 2022 pedrotech.com</p>
+      <p> &copy; 2022 kamariainman.com</p>
     </div>
   );
 }
